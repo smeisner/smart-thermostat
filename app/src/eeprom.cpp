7eb364eab48304f80fc66609c64b5cf8a9e67bf7
Steve Meisner <steve@meisners.net>
2023-06-26 07:16:39 -0400

    Create 2 namespaces for the preferences data

--- app/src/eeprom.cpp
+++ app/src/eeprom.cpp
@@ -3,8 +3,7 @@
 #include "wifi-credentials.h"
 #include <nvs_flash.h>
 
-Preferences wifiCreds;
-Preferences thermostat;
+Preferences prefs;
 
 //@@@
 //Timezone info, such as "America/New York"
@@ -29,90 +28,104 @@ void clearNVS()
 
 void setDefaultThermostatParams()
 {
-  thermostat.putInt("currMode", IDLE);
-  thermostat.putInt("setMode", OFF);
-  thermostat.putFloat("setTemp", 70.0);
-  thermostat.putFloat("setTempAutoMin",68.0);
-  thermostat.putFloat("setTempAutoMax",74.0);
-  thermostat.putFloat("currTemp", 0.0);
-  thermostat.putFloat("currHumid", 50.0);
-  thermostat.putChar("setUnits", 'F');
-  thermostat.putFloat("setSwing", 3.0);
-  thermostat.putFloat("setTempCorr", -4.8);
-  thermostat.putFloat("setHumidityCorr", 10);
-  thermostat.putInt("sleepTime", 30);
-  thermostat.putInt("timezoneSel", 17);
-  thermostat.putBool("Beep", true);
-  thermostat.end();
+  prefs.begin("thermostat", false);
+  prefs.putInt("currMode", IDLE);
+  prefs.putInt("setMode", OFF);
+  prefs.putFloat("setTemp", 70.0);
+  prefs.putFloat("setTempAutoMin",68.0);
+  prefs.putFloat("setTempAutoMax",74.0);
+  prefs.putFloat("currTemp", 0.0);
+  prefs.putFloat("currHumid", 50.0);
+  prefs.putChar("setUnits", 'F');
+  prefs.putFloat("setSwing", 3.0);
+  prefs.putFloat("setTempCorr", -4.8);
+  prefs.putFloat("setHumidityCorr", 10);
+  prefs.putInt("sleepTime", 30);
+  prefs.putInt("timezoneSel", 15);
+  prefs.putBool("Beep", true);
+  prefs.end();
 }
 
 void getThermostatParams()
 {
-  OperatingParameters.hvacOpMode = (HVAC_MODE)thermostat.getInt("currMode", (int)IDLE);
-  OperatingParameters.hvacSetMode = (HVAC_MODE)thermostat.getInt("setMode", (int)OFF);
-  OperatingParameters.tempSet = thermostat.getFloat("setTemp", 70.0);
-  OperatingParameters.tempSetAutoMin = thermostat.getFloat("setTempAutoMin", 68.0);
-  OperatingParameters.tempSetAutoMax = thermostat.getFloat("setTempAutoMax", 74.0);
-  OperatingParameters.tempCurrent = thermostat.getFloat("currTemp", 0.0);
-  OperatingParameters.humidCurrent = thermostat.getFloat("currHumid", 50.0);
-  OperatingParameters.tempUnits = thermostat.getChar("setUnits", 'F');
-  OperatingParameters.tempSwing = thermostat.getFloat("setSwing", 3.0);
-  OperatingParameters.tempCorrection = thermostat.getFloat("setTempCorr", -4.8);
-  OperatingParameters.humidityCorrection = thermostat.getFloat("setHumidityCorr", 10);
-  OperatingParameters.thermostatSleepTime = thermostat.getInt("sleepTime", 30);
-  OperatingParameters.timezone_sel = thermostat.getInt("timezoneSel", 17);
+  prefs.begin("thermostat", false);
+  OperatingParameters.hvacOpMode = (HVAC_MODE)prefs.getInt("currMode", (int)IDLE);
+  OperatingParameters.hvacSetMode = (HVAC_MODE)prefs.getInt("setMode", (int)OFF);
+  OperatingParameters.tempSet = prefs.getFloat("setTemp", 70.0);
+  OperatingParameters.tempSetAutoMin = prefs.getFloat("setTempAutoMin", 68.0);
+  OperatingParameters.tempSetAutoMax = prefs.getFloat("setTempAutoMax", 74.0);
+  OperatingParameters.tempCurrent = prefs.getFloat("currTemp", 0.0);
+  OperatingParameters.humidCurrent = prefs.getFloat("currHumid", 50.0);
+  OperatingParameters.tempUnits = prefs.getChar("setUnits", 'F');
+  OperatingParameters.tempSwing = prefs.getFloat("setSwing", 3.0);
+  OperatingParameters.tempCorrection = prefs.getFloat("setTempCorr", -4.8);
+  OperatingParameters.humidityCorrection = prefs.getFloat("setHumidityCorr", 10);
+  OperatingParameters.thermostatSleepTime = prefs.getInt("sleepTime", 30);
+  OperatingParameters.timezone_sel = prefs.getInt("timezoneSel", 15);
   OperatingParameters.timezone = (char *)gmt_timezones[OperatingParameters.timezone_sel];
-  OperatingParameters.thermostatBeepEnable = thermostat.getBool("Beep", true);
+  OperatingParameters.thermostatBeepEnable = prefs.getBool("Beep", true);
   OperatingParameters.lightDetected = 1024;
   OperatingParameters.motionDetected = false;
+  prefs.end();
 }
 
 void setDefaultWifiCreds()
 {
-  wifiCreds.putString("hostname", hostname);
-  wifiCreds.putString("ssid", wifiSsid);
-  wifiCreds.putString("pass", wifiPass);
+  prefs.begin("wificreds", false);
+  prefs.putString("hostname", hostname);
+  prefs.putString("ssid", wifiSsid);
+  prefs.putString("pass", wifiPass);
+  prefs.end();
 }
 
 void getWifiCreds()
 {
-  strncpy (WifiCreds.hostname, wifiCreds.getString("hostname", hostname).c_str(), sizeof(WifiCreds.hostname));
-  strncpy (WifiCreds.ssid, wifiCreds.getString("ssid", wifiSsid).c_str(), sizeof(WifiCreds.ssid));
-  strncpy (WifiCreds.password, wifiCreds.getString("pass", wifiPass).c_str(), sizeof(WifiCreds.password));
+  prefs.begin("wificreds", false);
+  strncpy (WifiCreds.hostname, prefs.getString("hostname", hostname).c_str(), sizeof(WifiCreds.hostname));
+  strncpy (WifiCreds.ssid, prefs.getString("ssid", wifiSsid).c_str(), sizeof(WifiCreds.ssid));
+  strncpy (WifiCreds.password, prefs.getString("pass", wifiPass).c_str(), sizeof(WifiCreds.password));
+  prefs.end();
 }
 
 bool eepromUpdateHvacSetTemp()
 {
-  thermostat.begin("thermostat", false);
-  thermostat.putFloat("setTemp", OperatingParameters.tempSet);
-  thermostat.end();
+  prefs.begin("thermostat", false);
+  prefs.putFloat("setTemp", OperatingParameters.tempSet);
+  prefs.end();
   return true;
 }
 
 bool eepromUpdateHvacSetMode()
 {
-  thermostat.begin("thermostat", false);
-  thermostat.putInt("currMode", OperatingParameters.hvacSetMode);
-  thermostat.end();
+  prefs.begin("thermostat", false);
+  prefs.putInt("currMode", OperatingParameters.hvacSetMode);
+  prefs.end();
   return true;
 }
 
 void eepromInit()
 {
-  wifiCreds.begin("wifiCreds", false);
-  thermostat.begin("thermostat", false);
-
-  if (thermostat.getChar("setUnits", 'X') == 'X')
+  prefs.begin("thermostat", false);
+  if ((not prefs.isKey("setUnits")) ||
+      (prefs.getChar("setUnits", 'X') == 'X'))
   {
     Serial.printf("Initializing EEPROM...\n");
+    prefs.end();
     setDefaultThermostatParams();
   }
+  else
+    prefs.end();
 
-  if (wifiCreds.getString("ssid", "") == "")
+  prefs.begin("wificreds", false);
+  if ((not prefs.isKey("ssid")) ||
+      (prefs.getString("ssid", "X") == "X"))
   {
     Serial.printf("Initializing stored wifi credentials...\n");
+    prefs.end();
     setDefaultWifiCreds();
   }
+  else
+    prefs.end();
 
   getThermostatParams();
   getWifiCreds();
