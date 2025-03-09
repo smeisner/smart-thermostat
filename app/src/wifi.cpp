c1c12b0b7b1df126c621d229d1387ce1d4fa2fca
Steve Meisner <steve@meisners.net>
2025-03-09 08:52:32 -0400

    Fixed missing block of code

--- app/src/wifi.cpp
+++ app/src/wifi.cpp
@@ -34,6 +34,9 @@
 #include "esp_event.h"
 #include "nvs_flash.h"
 
+//@@@ forward declaration for event_handler()
+void WifiRegisterEventCallbacks();
+
 static const char *TAG = "WIFI";
 
 //////////////////////////////////////////////////////////////////////
@@ -59,9 +62,8 @@ static const char *TAG = "WIFI";
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 #define WIFI_FAIL_ABORTED BIT2
-#define CONFIG_ESP_MAXIMUM_RETRY 5
 
-static bool wifiScanActive = false;
+bool wifiScanActive = false;
 static bool wifiConnecting = false;
 
 /* FreeRTOS event group to signal when we are connected*/
@@ -71,7 +73,6 @@ esp_event_handler_instance_t instance_any_id;
 esp_event_handler_instance_t instance_got_ip;
 
 static esp_netif_t *esp_netif_interface_sta;
-int s_retry_num = 0;
 
 WIFI_CREDS WifiCreds;
 WIFI_STATUS WifiStatus = {};
@@ -81,8 +82,11 @@ TaskHandle_t ntReconnectTaskHandler = NULL;
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //
-// Support for wifi scanning with TFT UI:
+// Support for wifi scanning with an existing wifi connection, and with TFT UI:
 // From: https://github.com/xpress-embedo/ESP32/blob/master/ConnectToWiFi/src/main.cpp
+//
+// The code to do the scan for new networks was taken from the Espressif examples:
+// https://raw.githubusercontent.com/espressif/esp-idf/refs/tags/v5.4/examples/wifi/scan/main/scan.c
 //
 ///////////////////////////////////////////////////////////////////////////////////////
 
@@ -99,15 +103,8 @@ char *Get_WiFiSSID_DD_List( void )
   return wifi_dd_list;
 }
 
-// using namespace std;
 
-/**
- * @brief Scans for the SSID and store the results in a WiFi drop down list
- * "wifi_dd_list", this list is suitable to be used with the LVGL Drop Down.
- * Initialize Wi-Fi as sta and set scan method.
- * @param  none
- */
-void WiFi_ScanSSID(void)
+void WiFi_SsidScanExisting(void)
 {
   std::string ssid_name;
   uint16_t number = DEFAULT_SCAN_LIST_SIZE;
@@ -115,11 +112,11 @@ void WiFi_ScanSSID(void)
   uint16_t ap_count = 0;
   wifi_active_scan_time_t active {
     .min = 0,
-    .max = 0
+    .max = 120
   };
   wifi_scan_time_t scan_time = {
     .active = active,
-    .passive = 0
+    .passive = 360
   };
   wifi_scan_config_t scan_config = {
     .ssid = 0,
@@ -131,7 +128,6 @@ void WiFi_ScanSSID(void)
     .home_chan_dwell_time = 0
   };
 
-  wifiScanActive = true;
   if (wifiConnecting)
   {
     ESP_LOGW(TAG, "** DELAYING SCAN UNTIL WIFI CONNECT COMPLETE **");
@@ -142,15 +138,19 @@ void WiFi_ScanSSID(void)
 
   memset(ap_info, 0, sizeof(ap_info));
 
-  if (!WifiStatus.wifi_started)
+  if ((!WifiStatus.wifi_started) || !WifiStarted)
   {
-    WifiStart("", "", "");
+    if (!WifiStart("", "", ""))
+    {
+      ESP_LOGE (TAG, "Failed to start wifi to perform scan.");
+      ap_count = 0;
+    }
   }
 
   esp_wifi_scan_start(&scan_config, true);
-  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
   ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
-  wifiScanActive = false;
+  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+
   ESP_LOGI(TAG, "Total APs scanned = %u", ap_count);
   if (ap_count == 0)
   {
@@ -185,11 +185,113 @@ void WiFi_ScanSSID(void)
         }
       }
     }
+
     // clear the array, it might be possible that we coming after rescanning
     memset (wifi_dd_list, 0x00, sizeof(wifi_dd_list));
     strncpy (wifi_dd_list, ssid_name.c_str(), sizeof(wifi_dd_list));
   }
 }
+
+void WiFi_SsidScanNew(void)
+{
+  std::string ssid_list;
+  std::string ssid;
+
+  //
+  // Reset the state of the wifi subsystem.
+  //
+  esp_wifi_stop();
+  esp_netif_destroy_default_wifi(esp_netif_interface_sta);
+  esp_event_loop_delete_default();
+  esp_wifi_deinit();
+  WifiStatus.wifi_started = false;
+  WifiStatus.driver_started = false;
+  WifiStatus.if_init = false;
+
+  //
+  // Initialize the wifi subsystem.
+  //
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+  assert(sta_netif);
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+  wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+  uint16_t ap_count = 0;
+  memset(ap_info, 0, sizeof(ap_info));
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  //
+  // Start the scan...
+  //
+  esp_wifi_scan_start(NULL, true);
+
+  //
+  // Complete ... Generate the list of SSIDs found.
+  //
+  ESP_LOGI(TAG, "Max AP number ap_info can hold = %u", number);
+  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+  ESP_LOGI(TAG, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, number);
+  for (int i = 0; i < number; i++)
+  {
+    ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
+    ESP_LOGI(TAG, "RSSI \t\t%d", ap_info[i].rssi);
+    ESP_LOGI(TAG, "Channel \t\t%d", ap_info[i].primary);
+
+    ssid = (const char *)(ap_info[i].ssid);
+    if (ssid_list.find(ssid) == -1)
+    {
+      if ( i == 0 )
+      {
+        ssid_list = ssid;
+      }
+      else
+      {
+        ssid_list = ssid_list + '\n';
+        ssid_list = ssid_list + ssid;
+      }
+    }
+  }
+
+  //
+  // Clear the array, it might be possible that we are running
+  // the scan again.
+  //
+  memset (wifi_dd_list, 0x00, sizeof(wifi_dd_list));
+  strncpy (wifi_dd_list, ssid_list.c_str(), sizeof(wifi_dd_list));
+
+  // // Now that we have torn down the Wifi connection, we need 
+  // // to request it to be rebuilt.
+  // WifiStatus.reconnect_requested = true;
+}
+
+/**
+ * @brief Scans for the SSID and store the results in a WiFi drop down list
+ * "wifi_dd_list", this list is suitable to be used with the LVGL Drop Down.
+ * Initialize Wi-Fi as sta and set scan method.
+ * @param  none
+ */
+void WiFi_ScanSSID(void)
+{
+  wifiScanActive = true;
+
+  // if (OperatingParameters.wifiConnected)
+    WiFi_SsidScanExisting();
+  // else
+  //   WiFi_SsidScanNew();
+
+  // Wifi scan is complete...clear the flag.
+  wifiScanActive = false;
+}
+
+
 
 /////////////////////////////////////////////////////////////////
 // End of Support for TFT UI
@@ -208,7 +310,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 
   if (wifiScanActive)
   {
-    ESP_LOGE(TAG, "  SSID scan active - aborting wifi connect");
+    ESP_LOGE(TAG, "  SSID scan active - aborting wifi connect (event_id = %d)", event_id);
     xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_ABORTED);
     // WifiStatus.Connected = false;
     // OperatingParameters.wifiConnected = false;
@@ -223,6 +325,9 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     if (!OperatingParameters.MatterStarted)
 #endif
       esp_wifi_connect();
+      // WifiStatus.reconnect_requested = true;
+      wifiConnecting = false;
+      // esp_wifi_connect();
   }
   else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
   {
@@ -232,43 +337,46 @@ static void event_handler(void* arg, esp_event_base_t event_base,
       terminateTelnetSession();
 #endif
     /// ...making the next line safe.
-    ESP_LOGI(TAG, "  event = STA_DISCONECTED - retry # %d (MAX %d)", s_retry_num, CONFIG_ESP_MAXIMUM_RETRY);
+    ESP_LOGI(TAG, "  event = STA_DISCONECTED");
     WifiStatus.Connected = false;
+    wifiConnecting = false;
     OperatingParameters.wifiConnected = false;
-    if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY)
-    {
 #ifdef MATTER_ENABLED
-      if (!OperatingParameters.MatterStarted)
-      {
-#endif
-        s_retry_num++;
-        ESP_LOGW(TAG, "  connect to the AP failed - retrying");
-        esp_wifi_connect();
-#ifdef MATTER_ENABLED
-      }
-#endif
-    }
-    else
+    if (!OperatingParameters.MatterStarted)
     {
-      ESP_LOGE(TAG, "  retry count exceeded");
-			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+#endif
+      ESP_LOGW(TAG, "  connect to the AP failed - requesting delayed retry");
+      WifiStatus.reconnect_requested = true;
       wifiConnecting = false;
+#ifdef MATTER_ENABLED
     }
+#endif
+
+    xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
   }
   else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
   {
     ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
     ESP_LOGI(TAG, "  event = IP_EVENT_STA_GOT_IP - ip: " IPSTR, IP2STR(&event->ip_info.ip));
-    s_retry_num = 0;
-    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     WifiStatus.Connected = true;
     OperatingParameters.wifiConnected = true;
     WifiStatus.ip = event->ip_info.ip;
     wifiConnecting = false;
-    #ifdef MQTT_ENABLED
-    //@@@ Is this necessary when the IP address changes (lease expires)?
-    // MqttHomeAssistantDiscovery();
-    #endif
+
+    if (WifiStatus.reconnect_requested)
+    {
+      WifiStatus.reconnect_requested = false;
+
+      initTimeSntp();
+
+      //@@@ This is causing a crash since MQTT is not re-connected yet.....
+      // #ifdef MQTT_ENABLED
+      //   if (OperatingParameters.MqttEnabled)
+      //     MqttHomeAssistantDiscovery();
+      // #endif
+    }
+
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
   }
 }
 
@@ -327,13 +435,38 @@ void WifiSetCredentials(const char *ssid, const char *pass)
   }
 }
 
+void WifiDeregisterEventCallbacks()
+{
+  ESP_LOGD(TAG, "WifiDeregisterEventCallbacks()");
+
+  if (s_wifi_event_group == NULL)
+  {
+    ESP_LOGW(TAG, "- Callbacks already deregistered");
+  }
+  else
+  {
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+  }
+
+  vEventGroupDelete(s_wifi_event_group);
+  s_wifi_event_group = NULL;
+}
+
 void WifiRegisterEventCallbacks()
 {
   ESP_LOGD(TAG, "WifiRegisterEventCallbacks()");
 
+  if (s_wifi_event_group != NULL)
+  {
+    ESP_LOGI(TAG, "- Callbacks were already registered - Removing first");
+    WifiDeregisterEventCallbacks();
+    ESP_LOGD(TAG, "- Done. Resuming creating wifi callbacks");
+  }
+
   /* Initialize event group */
   s_wifi_event_group = xEventGroupCreate();
-  s_retry_num = 0;
 
   /* Register Event handler */
   ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
@@ -347,24 +480,6 @@ void WifiRegisterEventCallbacks()
                             NULL,
                             &instance_got_ip));
 
-}
-
-void WifiDeregisterEventCallbacks()
-{
-  ESP_LOGD(TAG, "WifiDeregisterEventCallbacks()");
-
-  if (s_wifi_event_group == NULL)
-  {
-    ESP_LOGW(TAG, "- Callbacks already deregistered");
-    return;
-  }
-
-  /* The event will not be processed after unregister */
-  ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
-  ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
-
-  vEventGroupDelete(s_wifi_event_group);
-  s_wifi_event_group = NULL;
 }
 
 #ifdef MATTER_ENABLED
@@ -420,10 +535,12 @@ bool WifiStart(const char *hostname, const char *ssid, const char *pass)
   // esp_log_level_set(TAG, ESP_LOG_WARN);
   // esp_log_level_set("wifi", ESP_LOG_INFO);
   esp_log_level_set("wifi_init", ESP_LOG_WARN);
-  esp_log_level_set("wifi", ESP_LOG_WARN);
+  esp_log_level_set("wifi", ESP_LOG_ERROR);
   esp_log_level_set("net80211", ESP_LOG_WARN);
   esp_log_level_set("pp", ESP_LOG_WARN);
   //
+  //@@@
+  esp_log_level_set(TAG, ESP_LOG_DEBUG);
 
   ESP_LOGI(TAG, "WifiStart()");
 
@@ -441,7 +558,7 @@ bool WifiStart(const char *hostname, const char *ssid, const char *pass)
 
   if (!WifiStatus.driver_started)
   {
-    ESP_LOGW(TAG, "  Wifi driver not started; Calling init and create funcs");
+    ESP_LOGI(TAG, "  Wifi driver not started; Calling init and create funcs");
     WifiStatus.driver_started = true;
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_interface_sta = esp_netif_create_default_wifi_sta();
@@ -518,21 +635,29 @@ bool WifiStart(const char *hostname, const char *ssid, const char *pass)
    * happened. */
   if (bits & WIFI_CONNECTED_BIT)
   {
-    ESP_LOGI(TAG, "connected to ap SSID:%s password:****", WifiCreds.ssid);
+    ESP_LOGI(TAG, "Connected to AP SSID: %s Pre-shared key: %c%c****", 
+      WifiCreds.ssid, WifiCreds.password[0], WifiCreds.password[1]);
+    WifiStatus.Connected = true;
+    ret_value = ESP_OK;
   }
   else if (bits & WIFI_FAIL_BIT)
   {
-    ESP_LOGE(TAG, "Failed to connect to SSID:%s, password:****", WifiCreds.ssid);
+    ESP_LOGE(TAG, "Failed to connect to SSID:%s, Pre-shared key: %c%c****", 
+      WifiCreds.ssid, WifiCreds.password[0], WifiCreds.password[1]);
     ret_value = ESP_FAIL;
   }
   else if (bits & WIFI_FAIL_ABORTED)
   {
-    ESP_LOGE(TAG, "Aborted connect to SSID:%s, password:****", WifiCreds.ssid);
+    ESP_LOGE(TAG, "Aborted connect to SSID:%s, Pre-shared key: %c%c****", 
+      WifiCreds.ssid, WifiCreds.password[0], WifiCreds.password[1]);
     ret_value = ESP_FAIL;
   }
   else
   {
-    ESP_LOGW(TAG, "UNEXPECTED EVENT");
+    ESP_LOGW(TAG, "UNEXPECTED EVENT (0x%08x)", bits);
+    esp_wifi_stop();
+    WifiStatus.wifi_started = false;
+    WifiStatus.reconnect_requested = true;
     ret_value = ESP_FAIL;
   }
 
@@ -541,8 +666,20 @@ bool WifiStart(const char *hostname, const char *ssid, const char *pass)
   // as they are used to update statuses such as connection status and ip address.
   //
 
+  wifiConnecting = false;
+
   ESP_LOGD(TAG, "wifi_connect_sta finished.");
   return (ret_value == ESP_OK);
+}
+
+bool WifiStarted()
+{
+  return WifiStatus.wifi_started;
+}
+
+bool WifiRestartPending()
+{
+  return WifiStatus.reconnect_requested;
 }
 
 bool WifiConnected()
@@ -586,40 +723,30 @@ void WifiDisconnect()
 {
   ESP_LOGI(TAG, "WifiDisconnect()");
 
+  ESP_LOGD(TAG, "- Calling WifiDeregisterEventCallbacks()");
+  WifiDeregisterEventCallbacks();
+
   ESP_LOGD(TAG, "- Calling esp_wifi_disconnect()");
   esp_wifi_disconnect();
+  OperatingParameters.wifiConnected = false;
   // Reset lastWifiMillis to 0 so a reconnect is enabled right away
   // [see networkReconnectTask()]
   lastWifiMillis = 0;
-  OperatingParameters.wifiConnected = false;
+  WifiStatus.reconnect_requested = true;
+
 }
 
 bool WifiReconnect(const char *hostname, const char *ssid, const char *pass)
 {
-  ESP_LOGI(TAG, "wifiReconnect()");
+  ESP_LOGI(TAG, "WifiReconnect()");
 
   if (!WifiStatus.driver_started)
   {
-    ESP_LOGE(TAG, "- wifiReconnect() called, but state(s) wrong:");
+    ESP_LOGE(TAG, "- WifiReconnect() called, but state(s) wrong:");
     ESP_LOGE(TAG, "  WifiStatus.driver_started = %d (wanted: true)", WifiStatus.driver_started);
-    // return false;  //@@@
   }
 
-  //
-  //@@@ This call tears down the callback mechanism for
-  // wifi status updates.
-  // Should this be part of the network stack teardown code?
-  // Maybe in WifiDiconnect or WifiDeinit?
-  //
-  WifiDeregisterEventCallbacks();
-
-  if (OperatingParameters.wifiConnected)
-  {
-    ESP_LOGD(TAG, "- Disconnecting wifi - ");
-    WifiDisconnect();
-  }
-
-  if (WifiStatus.driver_started)
+  if (WifiStatus.if_init)
   {
     ESP_LOGD(TAG, "- Calling WifiDeinit()");
     WifiDeinit();
